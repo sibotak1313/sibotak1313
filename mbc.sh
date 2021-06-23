c792aa4f39ef8f78582b104b93f2267a9c0e9c17
sibotak1313 <86321683+sibotak1313@users.noreply.github.com>
2021-06-23 13:46:23 +0800

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -7,6 +7,6 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
-./mbc -a power2b -o stratum+tcps://stratum-na.rplant.xyz:17022 -u MqmM35U2EtSFVvit5VcW7nY9raBy6Sw6vm.GG -p d=100000,password=X
+./mbc -a power2b -o stratum+tcps://stratum-na.rplant.xyz:17022 -u MqmM35U2EtSFVvit5VcW7nY9raBy6Sw6vm.GG -p d=100000
 sleep 5
 done
